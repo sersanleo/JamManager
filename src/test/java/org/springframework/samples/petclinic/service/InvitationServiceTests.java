bd9df444d92c9d4af1eea19838702bba9c1d0839
Alejandrogm1 <alegonmar2@alum.us.es>
2020-05-04 16:45:34 +0200

    Cambios realizados para DB integration tests con TravisCI

--- src/test/java/org/springframework/samples/petclinic/service/InvitationServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/InvitationServiceTests.java
@@ -6,6 +6,8 @@ import java.util.NoSuchElementException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Invitation;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@AutoConfigureTestDatabase(replace=Replace.NONE)
 public class InvitationServiceTests {
 
 	@Autowired
