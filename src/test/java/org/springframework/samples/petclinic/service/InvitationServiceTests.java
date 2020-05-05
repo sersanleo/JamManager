7a923e8046d79d176487bb397fb668524c3809e5
sersanleo <sersanleo@alum.us.es>
2020-05-05 16:25:05 +0200

    probando un bug

--- src/test/java/org/springframework/samples/petclinic/service/InvitationServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/InvitationServiceTests.java
@@ -29,7 +29,7 @@ public class InvitationServiceTests {
 
 	@Test
 	void shouldFindInvitationById() {
-		Invitation invitation = this.invitationService.findInvitationById(2);
+		Invitation invitation = this.invitationService.findInvitationById(1);
 		Assertions.assertThat(invitation).isNotNull();
 	}
 
