ca73d28a499121eedba656beac68bdb1f2c2cb68
sersanleo <sersanleo@alum.us.es>
2020-05-10 04:53:28 +0200

    Finished HU01 UI Tests

--- src/test/java/org/springframework/samples/petclinic/bdd/stepdefinitions/AbstractStep.java
+++ src/test/java/org/springframework/samples/petclinic/bdd/stepdefinitions/AbstractStep.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,6 +15,8 @@ public class AbstractStep {
 
 	public WebDriver getDriver() {
 		if (driver == null) {
+			String pathToGeckoDriver = ".\\src\\main\\resources";
+			System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		}
@@ -27,5 +30,10 @@ public class AbstractStep {
 			fail(verificationErrorString);
 		}
 		driver = null;
+	}
+	
+	protected boolean isElementPresent(By by) {
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS); // Para que vaya mas rapido. No subir
+		return driver.findElements(by).size() > 0;
 	}
 }
