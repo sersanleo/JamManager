263d0d0db5623ca08ca622060a15e286125d3966
sersanleo <sersanleo@alum.us.es>
2020-05-29 04:50:02 +0200

    A ver si Travis va ya

--- src/test/java/org/springframework/samples/petclinic/web/e2e/MarkControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/MarkControllerE2ETests.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext
 class MarkControllerE2ETests {
 
 	private static final int TEST_RATING_JAM_ID = 4;
